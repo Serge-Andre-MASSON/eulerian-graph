6b47d7de2f03d0b46b6c1b4afa17a3f1f114e4bb
Serge <serge.andre.masson@gmail.com>
2023-06-29 22:43:07 +0200

    Update streamlit app content

--- streamlit_app/Le_problème.py
+++ streamlit_app/Le_problème.py
@@ -6,11 +6,12 @@ from eulerian_graph.graph import Graph
 
 
 st.set_page_config(page_title="Le problème")
+st.sidebar.markdown("# Les septs ponts de Koenigsberg")
 
 st.markdown("# Le problème")
 st.write("""Jusqu'en 1735, les habitants de Koenigsberg se demandent s'il est possible de visiter leur ville en empruntant chacun de ses ponts une et une seule fois.""")
 st.image("streamlit_app/Koenigsberg_bridges.png")
-st.write("""C'est finalement Leonard Euler qui apporte la réponse, négative, à cette question. Son approche pour résoudre le problème commence avec une modélisation du problème sous forme d'un graphe où chaque sommet est une berge et chaque arête est un pont: """)
+st.write("""C'est finalement Leonard Euler qui apporte la réponse, négative, à cette question. Son approche commence avec une modélisation du problème sous forme d'un graphe où chaque sommet est une berge et chaque arête un pont: """)
 
 
 G = Graph()
@@ -21,37 +22,24 @@ fig, ax = plt.subplots()
 draw(G, ax=ax)
 st.pyplot(fig)
 
-st.write("""Ce qu'on l'on cherche s'appelle une "chaine eulérienne": un chemin le long des arêtes du graphe, les utilisant toutes une seule fois. Si ce chemin revient à son point de départ alors on parle de "cycle eulerien".""")
+st.write("""Ce qu'on l'on cherche s'appelle une "chaine eulérienne": un chemin le long des arêtes du graphe, les utilisant toutes une seule fois. Si ce chemin revient à son point de départ alors on parle de "cycle eulerien". """)
 
-st.write("""Lorsqu'on parcourt un chemin sur un graphe et que l'on rencontre un sommet d'ordre $2n + 1$ (nombre d'arête(s) issue(s) du sommet), le seul moyen de parcourir une seule fois les $2n$ arêtes encore non utilisées est de faire $n$ aller-retour sur ce sommet jusqu'à s'y retrouver bloqué.""")
+st.write("""Euler remarque ensuite que l'existence d'un tel chemin dépend de la parité des ordres des sommets (nombre d'arêtes issues d'un sommet) du graphe: lorsqu'on parcourt un chemin sur un graphe et que l'on rencontre un sommet d'ordre $2n + 1$, le seul moyen de parcourir une seule fois les $2n$ arêtes encore non utilisées est de faire $n$ aller-retour sur ce sommet jusqu'à s'y retrouver bloqué.""")
 st.write("""On en déduit qu'un sommet d'ordre impair est soit le premier soit le dernier d'une chaîne. Il ne peut donc pas y en avoir plus que deux. Autrement dit""")
-st.markdown(
-    r"""$$ \text{G admet une chaîne eulerienne} \Rightarrow \text{G a au plus 2 sommets d'ordre impair} $$""")
+st.latex(
+    r"""\text{G admet une chaîne eulerienne} \Rightarrow \text{G a au plus 2 sommets d'ordre impair}""")
 
 st.write("""On peut réécrire cette condition sous la forme""")
-st.markdown(
-    r"""$$ \text{G admet une chaîne eulerienne} \Rightarrow \text{G a 0 ou 2 sommets d'ordre impair} $$""")
-st.write("""en utilisant le fait qu'un graphe quelconque ne peut avoir qu'un nombre pair de sommet d'ordre impair, ce qui est du au fait que la somme des ordres de tous les sommet du graphe est nombre d'arête multiplié par deux.""")
+st.latex(
+    r"""\text{G admet une chaîne eulerienne} \Rightarrow \text{G a 0 ou 2 sommets d'ordre impair}""")
+st.write("""en utilisant le fait qu'un graphe quelconque ne peut avoir qu'un nombre pair de sommets d'ordre impair puisque la somme des ordres de tous les sommets du graphe est le double du nombre d'arêtes.""")
 
 st.write("""On peut aussi écrire une condition nécessaire pour l'existence d'un cycle :""")
-st.markdown(
-    r"""$$ \text{G admet un cycle eulerien} \Rightarrow \text{G n'a aucun sommets d'ordre impair} $$""")
-st.write("""Pour s'en convaincre il suffit de voir qu'un cycle est un cas particulier de chaîne.""")
-
-st.write("""S'il y a deux sommets d'ordre impair, les points de départ et d'arrivée sont distincts donc ce n'est pas un cycle. La seule possibilité restante est qu'il n'y en ait aucun.""")
+st.latex(
+    r"""\text{G admet un cycle eulerien} \Rightarrow \text{G n'a aucun sommets d'ordre impair}""")
+st.write("""Pour s'en convaincre il suffit de voir qu'un cycle est un cas particulier de chaîne : s'il y a deux sommets d'ordre impair les points de départ et d'arrivée sont distincts autrement dit, ce n'est pas un cycle. La seule possibilité est donc qu'il n'y en ait aucun.""")
 
 
-st.write("""Le graphe modélisant le problème des ponts de Koenigsberg possédant quatre sommets d'ordre impair, il n'admet ni chaîne ni cycle et on ne peut donc pas visiter la ville en utilisant tous les ponts une seule fois.""")
+st.write("""Le graphe modélisant le problème des ponts de Koenigsberg possédant quatre sommets d'ordre impair, il n'admet ni chaîne ni cycle: on ne peut pas visiter la ville en utilisant tous les ponts une seule fois.""")
 
-
-# edges = ""
-# edges = st.text_input('Add edges ("u_0,v_0 u_1, v_1 ...")')
-
-
-# if edges:
-#     for edge in edges.split():
-#         u, v = [int(i) for i in edge.split(",")]
-#         G.add_edge(u, v)
-
-# G.draw(ax)
-# st.pyplot(fig)
+st.write("""En revanche, il suffirait d'ajouter ou de retirer une arête entre deux quelconques des sommets pour que la promenade devienne possible.""")
--- streamlit_app/pages/2_Algorithme_de_construction.py
+++ streamlit_app/pages/2_Algorithme_de_construction.py
@@ -2,9 +2,14 @@ from inspect import getsource
 import streamlit as st
 
 import streamlit as st
-
 from eulerian_graph.edge import Edge
-from eulerian_graph.walk import insert_list
+from eulerian_graph.graph import Graph
+
+from eulerian_graph.walk import recursive_walk, stack_walk
+
+
+st.set_page_config(page_title="Les algorithmes")
+st.sidebar.markdown("# Les septs ponts de Koenigsberg")
 
 
 def modelisation():
@@ -15,50 +20,48 @@ def modelisation():
 
     st.markdown("## Arêtes et sommets")
     st.write(
-        """Les sommets sont simplement représentés par des entiers et une arête n'étant que la donnée de deux sommets d'un graphe on peut la modéliser par un tuple de deux entiers.""")
+        """Les sommets sont représentés par des entiers et une arête n'étant que la donnée de deux sommets d'un graphe on peut la modéliser par un tuple de deux entiers.""")
 
-    st.write("""On surcharge la méthode __\_\_eq\_\___ pour que (u, v) = (v, u).""")
+    st.write("""On surcharge la méthode __\_\_eq\_\___ pour que (u, v) = (v, u). Cela exprime le fait que les arêtes peuvent être aprcourues dans les deux sens.""")
 
-    with open("eulerian_graph/edge.py") as f:
-        edge_code = f.read()
-    st.code(edge_code)
+    code = getsource(Edge)
+    st.code(code)
 
     st.markdown("## Graphe")
     st.write("""On modélise le graphe de la façon suivante""")
-    with open("eulerian_graph/graph.py") as f:
-        graph_code = ""
-        for line in f:
-            if "#" in line:
-                continue
-            graph_code += line
-    st.code(graph_code)
+
+    code = getsource(Graph)
+    st.code(code)
 
 
 def recursive():
-    from eulerian_graph.walk import recursive_walk
     st.markdown("""## Algorithme de Hierholzer""")
 
     st.write("""L'implémentation proposée ici est une réécriture de la méthode proposée par Carl Hierholzer pour prouver l'existence d'une chaine dans un graphe donné. Une version optimisée est proposée dans la section suivante.""")
 
     st.write("""Dans le cas où le graphe n'a aucun sommet impair on commence par choisir un sommet au hasard et on trace un chemin à partir de celui-ci. Puisque tous les sommets rencontrés en chemin sont d'ordre pair, le seul sommet sur lequel on peut être bloqué est le sommet de départ.""")
     st.write(
-        """On obtient alors un cycle $u_0,\\ u_1,\\ u_2,\\ \\text{ ... } u_n, u_0$.""")
+        """On obtient alors un cycle """)
+    st.latex(r"""u_0,\ u_1,\ u_2,\ \text{ ... } u_n, u_0""")
     st.write(
-        """Si toutes les arêtes ont été utilisées alors c'est fini. Sinon on repart de n'importe quel sommet $v_0 = u_i$ ayant encore des arếtes, ce qui permet d'obtenir un nouveau cycle $v_0,\\ v_1,\\ v_2,\\ \\text{ ... } v_m, v_0$ que l'on peut insérer dans le cycle précédent en lieu et place du sommet qui avait encore des arêtes :""")
-    st.write(
-        """ $ u_0,\\ u_1,\\ u_2,\\ \\text{ ... } u_{i-1}, v_0,\\ v_1,\\ \\text{ ... } v_m, v_0, u_{i+1} \\text{ ... } u_n, u_0$""")
+        """Si toutes les arêtes ont été utilisées alors c'est fini. Sinon on repart de n'importe quel sommet $v_0 = u_i$ du chemin déjà tracé ayant encore des arếtes, ce qui permet d'obtenir un nouveau cycle $v_0,\\ v_1,\\ v_2,\\ \\text{ ... } v_m, v_0$ que l'on peut insérer dans le cycle précédent en lieu et place du sommet qui avait encore des arêtes :""")
+    st.latex(
+        r"""u_0,\ u_1,\ u_2,\ \text{ ... } u_{i-1}, v_0,\ v_1,\ \text{ ... } v_m, v_0, u_{i+1} \text{ ... } u_n, u_0""")
 
     st.write("""On réitère alors le processus autant de fois que nécéssaire.""")
 
-    st.write("""Lorsque le graphe à deux sommets d'ordre impair, on choisit l'un de ces deux sommets comme point de départ et on trace un chemin à partir de celui-ci. Le seul sommet sur lequel on peut rester bloqué est l'autre sommet impair. Lorsque cela arrive, soit il n'y a plus d'arêtes disponible et c'est fini, soit il en reste et on repart de l'une d'entre elles.""")
-    st.write("""On note que les arêtes restantes (non utilisées) forment un graphe dans lequel tous les sommets sont d'ordre pair. On est donc ramené au cas précédent et on applique la même procédure""")
+    st.write("""Lorsque le graphe à deux sommets d'ordre impair, on choisit l'un de ces deux sommets comme point de départ et on trace un chemin à partir de celui-ci. Le seul sommet sur lequel on peut rester bloqué est l'autre sommet impair. Lorsque cela arrive, on note que les arêtes restantes (non utilisées) forment un graphe dans lequel tous les sommets sont d'ordre pair. On est donc ramené au cas précédent et on applique la même procédure à partir d'un sommet du chemin déjà tracé.""")
 
-    walk_code = getsource(recursive_walk)
-    st.code(walk_code)
+    code = getsource(recursive_walk)
+    st.code(code)
 
 
 def stack():
-    pass
+    st.markdown("""## Algorithme de Hierholzer optimisé""")
+    st.write("""Cette implémntation suivante repose sur l'utilisation d'une pîle. Les raîsons pour lesquelles cette algorithme fonctionne sont les mêmes que celles évoquées dans la section précédente.""")
+
+    code = getsource(stack_walk)
+    st.code(code)
 
 
 page_names_to_funcs = {
